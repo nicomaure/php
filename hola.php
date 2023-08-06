fd9866543a56e0709a42b98fcd3123576ec3689f
nicomaure <nicomau@gmail.com>
2023-08-06 19:01:42 -0300

    Cambios

--- hola.php
+++ hola.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Hola</title>
 </head>
 <body>
     <?php
-    echo "Hola Mundo <br>";
-    echo date("d/m/Y"); /* Y mayusculas muestra los 4 digitos del ano */
+    echo "Hola Mundo <br>"; //echo para imprimir por pantalla
+    echo date("d/m/Y"); // Y mayusculas muestra los 4 digitos del año
     ?>
 </body>
 </html>
