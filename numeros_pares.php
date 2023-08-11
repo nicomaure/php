1aca71c5afae04a287dffd79096af061a2921e49
nicomaure <nicomau@gmail.com>
2023-08-11 12:28:17 -0300

    Cambios

--- numeros_pares.php
+++ numeros_pares.php
@@ -1,36 +1,33 @@
 <?php
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 ?>
 
         <?php
-        /*
-            $contador = 1;
-        
-            while($contador <= 100){ 
-               if($contador % 2 == 0)
-                echo $contador. "<br>"; //para dar especios hacia abajo.
-                if ($contador == 60) {
-                    break;
-                  }
-                   
-                $contador ++; }
 
-            
-        */
-        
-    
-        
-                  
-            for ( $i = 0;$i <=100; $i+=2){
-            echo $i. "<br>";
-            if ($i == 60) {
+        $contador = 1;
+
+        while ($contador <= 100) {
+            if ($contador % 2 == 0) {
+                echo "$contador <br>"; //para dar especios hacia abajo y diferentes formas de imprimir
+            }
+            if ($contador == 60) {
                 break;
-              }
             }
 
-         
+            $contador++;
+        }
+
+
+        for ($i = 0; $i <= 100; $i += 2) {
+            echo $i . "<br>";
+            if ($i == 60) {
+                break;
+            }
+        }
+
+
         ?> 
     
