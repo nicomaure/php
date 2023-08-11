a92bfc993a092f61b6a83d5e0b802a36de001fe7
nicomaure <nicomau@gmail.com>
2023-08-11 10:57:47 -0300

    Cambios

--- numeros_pares.php
+++ numeros_pares.php
@@ -3,8 +3,33 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 
-$stock = 800;
-
-echo $stock . " es mayor";
-
 ?>
+
+        <?php
+            $contador = 1;
+        
+            while($contador <= 100){ 
+               if($contador % 2 == 0)
+                echo $contador. "<br>"; //para dar especios hacia abajo.
+                if ($contador == 60) {
+                    break;
+                  }
+                   
+                $contador ++; }
+
+            
+
+        
+    
+        /*
+                  
+            for ( $i = 0;$i <=100; $i+=2){
+            echo $i. "<br>";
+            if ($i == 60) {
+                break;
+              }
+            }
+
+        */  
+        ?> 
+    
