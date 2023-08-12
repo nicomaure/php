9b28feb3c45d386ce96e0b6782b586abb7dcd983
nicomaure <nicomau@gmail.com>
2023-08-12 16:34:01 -0300

    Cambios

---
+++ clinica_foreach.php
@@ -0,0 +1,90 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
+//Definicion de pacientes
+$aPacientes = array(); //Defino el array
+$aPacientes[] = array( //cargo 4 array con datos de paciente sobre el array pripal
+    "dni" => "37.765.012",
+    "nombre" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 81.50
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Juan Irraola",
+    "edad" => 28,
+    "peso" => 79
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Beatriz Ocampo",
+    "edad" => 50,
+    "peso" => 79
+);
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Clinica</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="text-center py-5">
+            <h1>Listado de pacientes</h1>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table border table-hover">
+                    <thead>
+                        <tr>
+                            <th>DNI</th>
+                            <th>Nombre y apellido</th>
+                            <th>Edad</th>
+                            <th>Peso</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        
+                        for ($i = 0; $i < count($aPacientes); $i++) {
+
+                        ?>
+                            <tr>
+                                <td><?php echo $aPacientes[$i]["dni"]; ?></td>
+                                <td><?php echo $aPacientes[$i]["nombre"]; ?></td>
+                                <td><?php echo $aPacientes[$i]["edad"]; ?></td>
+                                <td><?php echo $aPacientes[$i]["peso"]; ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+
+                    </tbody>
+
+                </table>
+
+            </div>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
