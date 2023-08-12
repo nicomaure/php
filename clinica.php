ff327ec498d423ef2edbffcfad3f122035f4bee0
nicomaure <nicomau@gmail.com>
2023-08-12 11:31:08 -0300

    Cambios

--- clinica.php
+++ clinica.php
@@ -3,4 +3,33 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ?>
-    
+   
+<?php
+//Definicion de pacientes
+$aPacientes = array();
+$aPacientes[] = array(
+    "dni" => "37.765.012",
+    "nombre" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 81.50
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Juan Irraola",
+    "edad" => 28,
+    "peso" => 79
+);
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombre" => "Beatriz Ocampo",
+    "edad" => 50,
+    "peso" => 79
+);
+
+?>
