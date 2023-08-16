188710a1db1afcd35459560eda239c9d855d2a32
nicomaure <nicomau@gmail.com>
2023-08-16 11:07:25 -0300

    Cambios

--- area_rectangulo.php
+++ area_rectangulo.php
@@ -7,12 +7,12 @@ error_reporting(E_ALL);
 <?php
 //Definición
 function calcularAreaRect($base, $altura){
-    $area = ($base * $altura);
+    return $area = ($base * $altura);
 
 }
 
 //uso
-echo "el area es". calcularAreaRect(100, 0.60)."<br>";
-echo "el area es". calcularAreaRect(800, 300)."<br>";
+echo "el area es ". calcularAreaRect(100, 0.60)."<br>";
+echo "el area es ". calcularAreaRect(800, 300);
 
 ?>
