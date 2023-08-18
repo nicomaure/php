fd2f1a5c83aa2a50d5041717b7a143486fe5f03c
nicomaure <nicomau@gmail.com>
2023-08-18 12:08:29 -0300

    Cambios

---
+++ empleados.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Empleados</title>
+</head>
+<body>
+    <main class="container">
+        <div class="text-center py-5">
+            <h1>Listado de empleados</h1>
+        </div>
+
+    </main>
+    
+</body>
+</html>
