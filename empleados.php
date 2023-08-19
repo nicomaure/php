fed4956159776dad6b92d715913f2581302d6bb2
nicomaure <nicomau@gmail.com>
2023-08-18 23:14:09 -0300

    Cambios

--- empleados.php
+++ empleados.php
@@ -4,10 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $aEmpleados = array();
-$aEmpleados[] = array("dni"=>33300123, "nombre"=>"David García", "Bruto"=>85000.30);
-$aEmpleados[] = array("dni"=>40874456, "nombre"=>"Ana del Valle", "Bruto"=>90000);
-$aEmpleados[] = array("dni"=>67567565, "nombre"=>"Andrés Perez", "Bruto"=>100000);
-$aEmpleados[] = array("dni"=>75744545, "nombre"=>"Victoria Luz", "Bruto"=>70000);
+$aEmpleados[] = array("dni" => 33300123, "nombre" => "David García", "Bruto" => 85000.30);
+$aEmpleados[] = array("dni" => 40874456, "nombre" => "Ana del Valle", "Bruto" => 90000);
+$aEmpleados[] = array("dni" => 67567565, "nombre" => "Andrés Perez", "Bruto" => 100000);
+$aEmpleados[] = array("dni" => 75744545, "nombre" => "Victoria Luz", "Bruto" => 70000);
 
 
 ?>
@@ -15,6 +15,7 @@ $aEmpleados[] = array("dni"=>75744545, "nombre"=>"Victoria Luz", "Bruto"=>70000)
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,6 +23,7 @@ $aEmpleados[] = array("dni"=>75744545, "nombre"=>"Victoria Luz", "Bruto"=>70000)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Empleados</title>
 </head>
+
 <body>
     <main class="container">
         <div class="text-center py-5">
@@ -38,27 +40,48 @@ $aEmpleados[] = array("dni"=>75744545, "nombre"=>"Victoria Luz", "Bruto"=>70000)
                         </tr>
                     </thead>
                     <tbody>
-                        
+
                         <?php
-                        foreach($aEmpleados as $empleado) {
+
+                        foreach ($aEmpleados as $empleado) {
 
                         ?>
                             <tr>
                                 <td><?php echo $empleado["dni"]; ?></td>
-                                <td><?php echo strtoupper($empleado["nombre"]); ?></td>
-                                <td><?php echo number_format($empleado["Bruto"], 2, "," , "."); ?></td>
+                                <td><?php echo mb_strtoupper($empleado["nombre"]); ?></td>
+                                <td><?php echo number_format(0.83 * $empleado["Bruto"], 2, ",", "."); ?></td>
                             </tr>
+
                         <?php
                         }
                         ?>
-                        
+
                     </tbody>
 
+
                 </table>
+                <div>
+                    <?php
+                    function contar($aEmpleados)
+                    {
+                        $cont = 0;
+                        foreach ($aEmpleados as $empleado) {
+                            $cont++;
+                        }
+                        return $cont;
+                    }
+                    echo "Cantidad de empleados son : " . contar($aEmpleados) . "<br>";
+                    ?>
+
+
+
+
+                </div>
             </div>
         </div>
 
     </main>
-    
+
 </body>
+
 </html>
