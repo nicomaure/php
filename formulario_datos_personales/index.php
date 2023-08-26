2b8243cc7d6b65fbf3ac085e122f32d4c47abed2
nicomaure <nicomau@gmail.com>
2023-08-26 08:47:08 -0300

    Cambios

--- formulario_datos_personales/index.php
+++ formulario_datos_personales/index.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Forumario Datos Personales</title>
 </head>
+
 <body>
     <main class="container">
         <div class="row">
@@ -45,6 +46,7 @@
 
 
     </main>
-    
+
 </body>
+
 </html>
--- formulario_datos_personales/resultado.php
+++ formulario_datos_personales/resultado.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-if($_POST){
+if ($_POST) {
     $nombre = trim($_POST["txtNombre"]);
     $dni = trim($_POST["txtDni"]);
     $telefono = trim($_POST["txtTelefono"]);
@@ -17,6 +17,7 @@ if($_POST){
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,12 +25,13 @@ if($_POST){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Resultado Formulario</title>
 </head>
+
 <body>
     <main class="container">
         <div class="row">
             <div class="col-12 text-center py-5">
                 <h1>Resultado Formulario</h1>
-            </div>           
+            </div>
         </div>
         <div class="row">
             <div class="col-12">
@@ -53,12 +55,13 @@ if($_POST){
                         </tr>
                     </tbody>
                 </table>
-                
+
 
             </div>
         </div>
 
     </main>
-    
+
 </body>
+
 </html>
