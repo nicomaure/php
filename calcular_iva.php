c031e2306e47ac9ce36087ae072397cb1e64d40a
nicomaure <nicomau@gmail.com>
2023-08-26 21:34:06 -0300

    Cambios

---
+++ calcular_iva.php
@@ -0,0 +1,31 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+if ($_POST){
+
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Calcular Iva</title>
+</head>
+<body>
+    <main class="container">
+
+    </main>
+    
+</body>
+</html>
