6b9f61c44be7e8f4b197c37e8d50b0468e415086
nicomaure <nicomau@gmail.com>
2023-09-28 10:39:25 -0300

    Cambios

--- gestordetareas/index.php
+++ gestordetareas/index.php
@@ -13,6 +13,8 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <title>Gestor de tareas</title>
 </head>
 <body>
