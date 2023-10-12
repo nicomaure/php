53d16db325202833e1adeee14e3233ef4fed27f5
nicomaure <nicomau@gmail.com>
2023-10-12 07:34:42 -0300

    Cambios

---
+++ formulario_invitados/index.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Formulario de Invitados</title>
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Lista de invitados</h1>
+            </div>
+
+        </div>
+
+    </main>
+    
+</body>
+</html>
