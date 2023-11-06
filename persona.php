08fba33604da3f5eea9c3e2d3454f93f34017c62
nicomaure <nicomau@gmail.com>
2023-11-05 22:49:39 -0300

    Cambios

--- persona.php
+++ persona.php
@@ -4,6 +4,57 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-ioioio
+class Persona{
+    public $dni;
+    public $nombre;
+    public $edad;
+    public $nacionalidad;
 
+    public function imprimir(){
+
+    }
+}
+
+class Alumno extends Persona{
+
+    public $legajo;
+    public $notaPorfolio;
+    public $notaPhp;
+    public $notaProyecto;
+
+    public function __construct()
+    {
+        $this->notaPorfolio= 0.0;
+        $this->notaPhp= 0.0;
+        $this->notaProyecto= 0.0;
+    }
+    
+
+    public function imprimir(){
+
+    }
+
+    public function calcularPromedio(){
+
+    }
+}
+
+class Docente extends Persona{
+    public $especialidad;
+
+    public function imprimir()
+    {
+        
+    }
+
+    public function imprimirEspecialidadesHabilitadas(){
+        
+    }
+}
+
+// Programa
+
+$alumno1 = new Alumno();
+$alumno1 ->promedio= 8;
+$alumno1 ->nombre= "Nico";
 ?>
