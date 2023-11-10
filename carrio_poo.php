08191f3790a6556673cd034e4d88665b9396471c
nicomaure <nicomau@gmail.com>
2023-11-09 23:36:00 -0300

    Cambios

---
+++ carrio_poo.php
@@ -0,0 +1,73 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+class Cliente{
+    private $dni;
+    private $nombre;
+    private $correo;
+    private $telefono;
+    private $descuento;
+
+    public function __construct()
+    {
+        $this->descuento = 0.0;
+    }
+
+    public function imprimir(){
+        echo "DNI: " . $this->dni . "<br>";
+        echo "Nombre: " . $this->nombre . "<br>";
+        echo "Correo: " . $this->correo . "<br>";
+        echo "Telefono: " . $this->telefono . "<br>";
+        echo "Descuento: " . $this->descuento . "<br>";
+    }
+
+}
+
+class Producto{
+    private $cod;
+    private $nombre;
+    private $precio;
+    private $descripcion;
+    private $iva;
+
+    public function __construct()
+    {
+        $this->precio = 0.0;
+        $this->iva = 0.0;
+    }
+
+    public function imprimir(){
+        echo "cod: " . $this->cod . "<br>";
+        echo "Nombre: " . $this->nombre . "<br>";
+        echo "Precio: " . $this->precio . "<br>";
+        echo "Descripcion: " . $this->descripcion . "<br>";
+        echo "iva: " . $this->iva . "<br>";
+    }
+
+
+}
+
+class Carrito{
+    private $cliente;
+    private $aProductos;
+    private $subtotal;
+    private $total;
+
+    public function __construct()
+    {
+        $this->aProductos = array();
+        $this->subtotal = 0.0;
+        $this->total = 0.0;
+    }
+
+    public function cargarProducto(){}
+
+    public function imprimirTicket(){}
+
+}
+
+
+?>
