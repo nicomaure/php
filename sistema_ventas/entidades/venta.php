f06e447a2d929d95a846ae933904756daefa8460
nicomaure <nicomau@gmail.com>
2023-12-03 08:30:19 -0300

    Cambios

--- sistema_ventas/entidades/venta.php
+++ sistema_ventas/entidades/venta.php
@@ -148,7 +148,12 @@ class Venta {
                 $entidadAux->idventa = $fila["idventa"];
                 $entidadAux->fk_idcliente = $fila["fk_idcliente"];
                 $entidadAux->fk_idproducto = $fila["fk_idproducto"];
-                $entidadAux->fecha = $fila["fecha"];
+                if(isset($fila["fecha"])){
+                    $entidadAux->fecha = $fila["fecha"];
+                }else{
+                    $entidadAux->fecha = ""; 
+                }
+                
                 $entidadAux->cantidad = $fila["cantidad"];
                 $entidadAux->preciounitario = $fila["preciounitario"];
                 $entidadAux->total = $fila["total"];
